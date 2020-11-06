96382495c9cba7e9812b502fd1364bd2b959edf4
dundermifflinns <74021488+dundermifflinns@users.noreply.github.com>
2020-11-06 23:06:39 +0200

    Add files via upload

---
+++ de-v4-b/counter.js
@@ -0,0 +1,44 @@
+var yandex = {
+    "v1": 51601805,
+    "v2": 51602411,
+    "v3": 51602435,
+    "v4": 51602471,
+    "v5": 51602510,
+    "v6": 51602558
+};
+
+function $_GET(key) {
+    var s = window.location.search;
+    s = s.match(new RegExp(key + '=([^&=]+)'));
+    return s ? s[1] : '';
+}
+
+var site = $_GET('version').replace('de', '').replace('49', '')
+
+if (!site) {
+    var site = document.location.pathname
+        .replace(/\/+/g, '')
+        .replace('index.html', '')
+        .replace('index2.html', '')
+        .replace('prelp', '')
+        .replace('prep', '')
+        .replace('de', '')
+        .replace('49', '')
+        .replace('confirm.html', '');
+
+}
+(function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+    };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(yandex[site], "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+});
